1f8006dd5022928e74b68ceb0cd90e1c2e31fc79
Wang Qiu <winsonwq@gmail.com>
2012-09-04 16:38:29 +0800

    push to mingming's side. Qiu

--- app/assets/javascripts/controllers/tasks.controller.js
+++ app/assets/javascripts/controllers/tasks.controller.js
@@ -1,11 +1,14 @@
-function TasksController($scope, $element) {
-	
-	$scope.tasks = [];
+function TasksController($scope, $element, $http) {
 
 	var addTaskForm = $element.find('#add-task-form');
 	addTaskForm.ajaxForm($scope.submitForm);
 
-	$scope.submitForm = function(){
-		console.log(arguments);
+	$scope.submitForm = function(json){
+		$scope.tasks.concat([json]);
+	};
+
+	$scope.init = function(lessonplanId){
+		$scope.Tasks = $resource('/lessonplans/:lessonplanId/tasks/:taskId', 
+			{ lessonplanId : lessonplanId, taskId : '@id' });
 	};
 }
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,20 @@
 class TasksController < ApplicationController
 
-	def index
+	before_filter :load_lessonplan
+
+	def load_lessonplan
 		lessonplan_id = params[:lessonplan_id]
+		@lessonplan = Lessonplan.find(lessonplan_id) unless lessonplan_id.blank? 
+	end
+
+	def index
+		render :json => @lessonplan.tasks unless @lessonplan.nil?
 	end
 
 	def create
-		lessonplan_id = params[:lessonplan_id]
-		lessonplan = Lessonplan.find(lessonplan_id) unless lessonplan_id.blank?
-		unless lessonplan.nil? then
+		unless @lessonplan.nil? then
 			task = Task.new params[:task]
-			task.lessonplan = lessonplan
+			task.lessonplan = @lessonplan
 			task.save
 
 			render :json => task
