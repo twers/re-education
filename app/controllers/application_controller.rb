3db9b20c2aa3e4530ab7b36e0d084840da9f4ac1
Wang Qiu <winsonwq@gmail.com>
2012-10-12 14:59:23 +0800

    add 404 page. Qiu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery
 
+	rescue_from ActionController::RoutingError,       :with => :render_404
+	rescue_from ActionController::UnknownController,  :with => :render_404
+	rescue_from ActionController::UnknownAction,      :with => :render_404
+	rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+	def render_404
+	  redirect_to '/404.html'
+	end  
+
 	def current_user
 		session[:user]
 	end
