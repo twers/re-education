9c5bd7732d360928b3543beba86e0e19a06867c7
Wang Qiu <winsonwq@gmail.com>
2012-11-01 00:12:04 +1100

    update migration script. Qiu

--- db/migrate/20120827042711_create_lessonplans.rb
+++ db/migrate/20120827042711_create_lessonplans.rb
@@ -2,7 +2,6 @@ class CreateLessonplans < ActiveRecord::Migration
   def change
     create_table :lessonplans do |t|
       t.string   "title"
-      t.integer  "student_count"
       t.string   "content"
       t.timestamps
     end
--- db/migrate/20120830133910_remove_student_count_from_lessonplans.rb
+++
@@ -1,9 +0,0 @@
-class RemoveStudentCountFromLessonplans < ActiveRecord::Migration
-  def up
-  	remove_column :Lessonplans, :student_count
-  end
-
-  def down
-  	add_column :Lessonplans, :student_count, :string
-  end
-end
