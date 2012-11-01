c825c6939415a6671ab75f8d2886cd80bc63a57f
Wang Qiu <winsonwq@gmail.com>
2012-11-01 22:34:33 +1100

    remove comment. Qiu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,6 @@ class ApplicationController < ActionController::Base
 	
 	rescue_from CanCan::AccessDenied do |exception|
 	  flash[:error] = "Access denied."
-    p "**********************"
-    p "Access denied."
-    p exception.inspect
-    p "**********************"
 	  redirect_to root_url
 	end
 
