fbfb490cb4853f3cd8a98dbab3fe4050806613bf
Wang Qiu <winsonwq@gmail.com>
2012-11-29 15:25:13 +0800

    re-style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,11 @@ module ApplicationHelper
 	def feature_on?
 		true
 	end
+
+	def colorful_icon_class_from_index idx
+		colorful_icons = ["pink", "green", "blue", "orange"]
+		colorful_text = ["&#127891;", "&#128640;", "&#128240;", "&#128101;", ""]
+		idx = idx % colorful_icons.length
+		{ :class_name => colorful_icons[idx], :icon_text => colorful_text[idx] }
+	end
 end
