552b74f211128b137ff0fa37593412d61325d641
Haiyang He <hyhe@thoughtworks.com>
2013-01-28 14:21:03 +0800

    add build.sh to run the ut and server

---
+++ build.sh
@@ -0,0 +1,50 @@
+function install_bundle {
+	bundle install --system --without production
+}
+
+function build_local {
+	install_bundle && \
+	migrate && \
+	RAILS_ENV=test rake test && \
+	RAILS_ENV=test bundle exec rspec
+}
+
+function ut {
+	migrate && \
+	RAILS_ENV=test rake test && \
+	RAILS_ENV=test bundle exec rspec
+}
+
+function migrate {
+	RAILS_ENV=test rake db:drop db:create db:migrate db:seed
+}
+
+function server {
+	RAILS_ENV=test rake db:drop db:create db:migrate db:seed && \
+	RAILS_ENV=test rails s
+}
+
+
+function usage {
+	printf "
+options:
+       h: Display this
+       m: Migrate Database
+      ut: Run Unit Test only
+       s: Start Server
+       *: Run local build script
+
+"
+}
+
+function main {
+  	case $1 in
+		h) usage ;;
+		ut) ut ;;
+		m) migrate ;;
+		s) server ;;
+		*) build_local ;;
+	esac
+}
+
+main $@
