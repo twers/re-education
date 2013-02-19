e7db987aba6d3aab331071cd16f82a6a5803d1ed
soloara <soloara87@gmail.com>
2013-02-20 00:26:28 +0800

    Add only options to route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ReEducation::Application.routes.draw do
   match 'login' => 'sessions#new', :as => 'login', :via => :get
   match 'logout' => 'sessions#destroy', :as => 'logout', :via => :get
 
-  resources :sessions, :only => [:new, :create]
+  resources :sessions, :only => [:create]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -20,14 +20,14 @@ ReEducation::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :lessonplans do
-    resources :tasks
-    resources :comments
-    resources :lessonplan_attachments, :as => 'attachments', :path => 'attachments'
+    resources :tasks, :only => [:index, :create]
+    resources :comments, :only => [:index, :create, :destroy]
+    resources :lessonplan_attachments, :as => 'attachments', :path => 'attachments', :only => [:index, :create, :destroy]
   end
 
-  resources :publishers
+  resources :publishers, :except => [:index]
   resources :users
-  resources :feedbacks
+  resources :feedbacks, :only => [:new, :create]
   
   # Sample resource route with options:
   #   resources :products do
