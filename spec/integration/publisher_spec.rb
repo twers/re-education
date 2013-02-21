6db6a18aa6b8a7499dc5d663c0a5b78ea9a20131
Wang Qiu <winsonwq@gmail.com>
2013-02-21 15:48:58 +0800

    format js file. Qiu

---
+++ spec/integration/publisher_spec.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+require 'spec_helper'
+
+describe "publisher related functions", :type => :feature do
+  let(:publisher) { FactoryGirl.create(:publisher) }
+
+  context "login" do
+
+  end
+
+end
