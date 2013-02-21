8b5821bb5f26070421b22f3d95fe3f65c1caed14
Wang Qiu <winsonwq@gmail.com>
2013-02-21 20:52:35 +0800

    remove unnecessary file. Qiu

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,4 @@ require 'spec_helper'
 
 describe UsersController do
 
-  describe :create do
-    it "returns http success"
-    #   post :create
-    #   response.should be_success
-    # end
-  end
-
 end
