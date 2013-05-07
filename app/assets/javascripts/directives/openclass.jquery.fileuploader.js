bb4282d7db942e91a2b62ddebf0889c8b0054244
Wang Qiu <winsonwq@gmail.com>
2013-05-07 10:20:54 +0800

    integrate jquery uploader plugin to qiniu service. Qiu

--- app/assets/javascripts/directives/openclass.jquery.fileuploader.js
+++ app/assets/javascripts/directives/openclass.jquery.fileuploader.js
@@ -1,24 +1,68 @@
 angular.module('openClass.directives')
-  .directive('jqueryFileUploader', ['eventConstants', function factory(eventConstants) {
+  .directive('jqueryFileUploader',
+    ['eventConstants', 'base64',
+      function factory(eventConstants, base64) {
 
-    var directive = {
-      restrict: 'C',
-      link: function (scope, element, attrs) {
-        var uploader = $(element).fileupload({
-          url: '/uploads.json',
-          type: 'POST',
-          sequentialUploads: true,
-          acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-          formData: {
-            _method : 'POST'
+        var auth = $('[name="qiniu-request-token"]').attr('content'),
+          qiniuDomain = 'http://newclass.qiniudn.com';
+
+        function getFormData(form) {
+          return [
+            {
+              name: "auth",
+              value: auth
+            },
+            {
+              name: "action",
+              value: form.find('[name="action"]').val()
+            }
+          ];
+        }
+
+        function addActionFormField(form) {
+          var rsput = form.find('[name="action"]');
+          if (rsput.length == 0) {
+            rsput = $('<input type="hidden" name="action"/>').appendTo(form);
           }
-        });
+          return rsput;
+        }
 
-        uploader.bind('fileuploaddone', function (e, data) {
-          scope.$emit(eventConstants.FILE_UPLOAD_DONE, data.result);
-        });
-      }
-    };
-    return directive;
+        function constructFiles(resultFiles) {
+          var files = [];
+          $(resultFiles).each(function (idx, item) {
+            var baseUrl = qiniuDomain + '/' + item.name;
+            files.push({
+              url: baseUrl,
+              mediumUrl: baseUrl + '?imageView/2/w/400',
+              thumbnailUrl: baseUrl + '?imageView/2/w/200'
+            });
+          });
+          return files;
+        }
 
-  }]);
+        var directive = {
+          restrict: 'C',
+          link: function (scope, element, attrs) {
+            var uploader = $(element).fileupload({
+              url: 'http://up.qbox.me/upload',
+              type: 'POST',
+              sequentialUploads: true,
+              acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+              formData: getFormData
+            });
+
+            uploader.bind('fileuploadsubmit', function (e, data) {
+              var rsput = addActionFormField(data.form),
+                filename = data.files[0].name;
+
+              rsput.val('/rs-put/' + base64.URLSafeBase64Encode('newclass:' + filename));
+            });
+
+            uploader.bind('fileuploaddone', function (e, data) {
+              var files = constructFiles(data.files);
+              scope.$emit(eventConstants.FILE_UPLOAD_DONE, { files: files });
+            });
+          }
+        };
+        return directive;
+      }]);
